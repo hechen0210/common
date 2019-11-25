6702c5a25b1e9530093b8298d25b05ce11da88a2
hechen <hechen@ysbjob.com>
2019-11-25 17:40:07 +0800

    mod

--- main.go
+++ main.go
@@ -6,47 +6,40 @@
 */
 package main
 
-import (
-	"common/config"
-	"common/logger"
-	"common/mysql"
-	"fmt"
-)
-
 func main() {
-	c := config.ConfigFile{
-		Path: "./config",
-		File: "",
-	}
-	data := c.Load()
-	config := data.GetSection("urls")
-	a := config.Get("inside").ToSString()
-	for key, item := range a {
-		fmt.Println(key, item)
-	}
-	fileLogger, err := logger.Log{
-		Path:     "./logs",
-		FileName: "log.log",
-		Rotate: logger.Rotate{
-			Type:   logger.Dir,
-			Period: logger.Daily,
-		},
-	}.NewFileLogger()
-	fileLogger.SetBaseContent("进入结算").Write("订单号").Print()
-	fileLogger.Write("aiyaya")
-	mc := mysql.Config{
-		Host:          "192.168.0.235",
-		User:          "root",
-		Password:      "ysb123456",
-		Port:          "3306",
-		DbName:        "new_ysb",
-		Prefix:        "ysb_",
-		SingularTable: false,
-	}.New()
-	if err != nil {
-		fileLogger.SetBaseContent("").Write(err.Error())
-	}
-	var cc []string
-	r := mc.Db.Raw("select * from ysb_users12").Scan(&cc)
-	fmt.Println(r.Error)
+	//c := config.ConfigFile{
+	//	Path: "./config",
+	//	File: "",
+	//}
+	//data := c.Load()
+	//config := data.GetSection("urls")
+	//a := config.Get("inside").ToSString()
+	//for key, item := range a {
+	//	fmt.Println(key, item)
+	//}
+	//fileLogger, err := logger.Log{
+	//	Path:     "./logs",
+	//	FileName: "log.log",
+	//	Rotate: logger.Rotate{
+	//		Type:   logger.Dir,
+	//		Period: logger.Daily,
+	//	},
+	//}.NewFileLogger()
+	//fileLogger.SetBaseContent("进入结算").Write("订单号").Print()
+	//fileLogger.Write("aiyaya")
+	//mc := mysql.Config{
+	//	Host:          "192.168.0.235",
+	//	User:          "root",
+	//	Password:      "ysb123456",
+	//	Port:          "3306",
+	//	DbName:        "new_ysb",
+	//	Prefix:        "ysb_",
+	//	SingularTable: false,
+	//}.New()
+	//if err != nil {
+	//	fileLogger.SetBaseContent("").Write(err.Error())
+	//}
+	//var cc []string
+	//r := mc.Db.Raw("select * from ysb_users12").Scan(&cc)
+	//fmt.Println(r.Error)
 }
