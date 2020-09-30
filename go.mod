093e03df1b3d7039cb9a8aabd7c20b1ed59624d3
hechen0210 <hechen0210@gmail.com>
2020-09-30 16:46:56 +0800

    test

--- go.mod
+++ go.mod
@@ -3,13 +3,12 @@ module github.com/hechen0210/common
 go 1.12
 
 require (
-	github.com/go-mgo/mgo v0.0.0-20180705113738-7446a0344b78 // indirect
-	github.com/go-redis/redis v6.15.6+incompatible // indirect
+	github.com/go-redis/redis v6.15.6+incompatible
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/jinzhu/gorm v1.9.11
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/shopspring/decimal v0.0.0-20191009025716-f1972eb1d1f5
-	github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271 // indirect
-	gopkg.in/ffmt.v1 v1.5.3
-	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+	github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 	gopkg.in/yaml.v2 v2.2.5
 )
--- main.go
+++ main.go
@@ -8,45 +8,55 @@ package main
 
 import (
 	"fmt"
-	"github.com/hechen0210/common/config"
-	"github.com/hechen0210/common/logger"
+	"github.com/hechen0210/common/mysql"
 )
 
 func main() {
-	c := config.ConfigFile{
-		Path: "./config",
-		File: "",
+	//c := config.ConfigFile{
+	//	Path: "./config",
+	//	File: "",
+	//}
+	//data := c.Load()
+	//config := data.GetSection("urls")
+	//a := config.Get("inside").ToSString()
+	//for key, item := range a {
+	//	fmt.Println(key, item)
+	//}
+	//fileLogger, err := logger.Log{
+	//	Path:     "./logs",
+	//	FileName: "log.log",
+	//	Rotate: logger.Rotate{
+	//		Type:   logger.Dir,
+	//		Period: logger.Daily,
+	//	},
+	//}.NewFileLogger()
+	//fileLogger.SetBaseContent("进入结算").Write("订单号").Print()
+	//fileLogger.Write("aiyaya")
+	//fmt.Println(err)
+	mc := mysql.Config{
+		Host:          "127.0.0.1",
+		User:          "root",
+		Password:      "",
+		Port:          "3306",
+		DbName:        "7dayWechat",
+		Prefix:        "7day_",
+		SingularTable: false,
+	}.New()
+	var admin struct {
+		Id        int    `json:"id"`
+		Account   string `json:"account"`
+		Password  string `json:"-"`
+		Name      string `json:"name"`
+		CreatedAt string `json:"created_at"`
+		UpdatedAt string `json:"updated_at"`
 	}
-	data := c.Load()
-	config := data.GetSection("urls")
-	a := config.Get("inside").ToSString()
-	for key, item := range a {
-		fmt.Println(key, item)
-	}
-	fileLogger, err := logger.Log{
-		Path:     "./logs",
-		FileName: "log.log",
-		Rotate: logger.Rotate{
-			Type:   logger.Dir,
-			Period: logger.Daily,
-		},
-	}.NewFileLogger()
-	fileLogger.SetBaseContent("进入结算").Write("订单号").Print()
-	fileLogger.Write("aiyaya")
+	c := mc.Client
+	err := c.Debug().Find(&admin)
 	fmt.Println(err)
-	//mc := mysql.Config{
-	//	Host:          "192.168.0.235",
-	//	User:          "root",
-	//	Password:      "ysb123456",
-	//	Port:          "3306",
-	//	DbName:        "new_ysb",
-	//	Prefix:        "ysb_",
-	//	SingularTable: false,
-	//}.New()
 	//if err != nil {
 	//	fileLogger.SetBaseContent("").Write(err.Error())
 	//}
 	//var cc []string
-	//r := mc.Db.Raw("select * from ysb_users12").Scan(&cc)
+	//r := mc.Client.Raw("select * from ysb_users12").Scan(&cc)
 	//fmt.Println(r.Error)
 }
