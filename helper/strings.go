b28411f403e01ce3ff61015d2634b66d968634ee
hechen0210 <hechen0210@gmail.com>
2020-10-06 02:43:09 +0800

    随机字符串

--- helper/strings.go
+++ helper/strings.go
@@ -1,6 +1,10 @@
 package helper
 
-import "strings"
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
 
 func SubStrByLen(str string, start, length int) string {
 	rs := []rune(str)
@@ -76,4 +80,46 @@ func HumpToUnder(str string) string {
 		}
 	}
 	return back
+}
+
+/**
+生成随机字符串
+*/
+
+func RandomString(l int, category int) (s string) {
+	var fullStr string
+	numStr := "1234567890"
+	uperStr := "ABCDEFGHIJKLMNOPQRSTUVWXZY"
+	lowerStr := "abcdefghijklmnopqrstuvwxz"
+	switch category {
+	case 1:
+		fullStr = numStr
+	case 2:
+		fullStr = uperStr
+	case 3:
+		fullStr = lowerStr
+	case 4:
+		fullStr = numStr + uperStr
+	case 5:
+		fullStr = numStr + lowerStr
+	case 6:
+		fullStr = uperStr + lowerStr
+	case 7:
+		fullStr = numStr + uperStr + lowerStr
+	default:
+		fullStr = numStr
+	}
+	_rand := []rune(fullStr)
+	randSeed := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		r := randSeed.Intn((l - 1))
+		var k int
+		if r > l {
+			k = r % l
+		} else {
+			k = r
+		}
+		s += string(_rand[k])
+	}
+	return s
 }
